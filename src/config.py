ce66ec428d7486e12d7a000c97e0483b42f99a4f
nmakes <nav.naveenvenkat@gmail.com>
2017-12-26 21:52:07 +0000

    making the repository usable for any website

--- src/config.py
+++ src/config.py
@@ -1,4 +1,8 @@
+# Root adress of the website to be scraped
 _root = 'https://www.stackoverflow.com/'
+
+# Additional interesting pages
 _featured = 'https://stackoverflow.com/?tab=featured'
 
+# Default meta file
 metaFileName = 'meta.data'
