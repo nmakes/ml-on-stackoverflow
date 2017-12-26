87d98ccf5a8db03005dc2a8f5699af514cec4885
nmakes <nav.naveenvenkat@gmail.com>
2017-12-26 21:54:38 +0000

    renaming to scraper

--- src/ml.py
+++ src/ml.py
@@ -1,18 +1,18 @@
-import scrapper
+import scraper
 import config
 
-meta = scrapper.MetaData()
+meta = scraper.MetaData()
 
 def get_all_featured_questions(meta):
 	
-	featuredPage = scrapper.url_to_soup(config._featured, meta)
+	featuredPage = scraper.url_to_soup(config._featured, meta)
 	fpSoup = featuredPage['soup']
 
 	allLinks = fpSoup.find_all('a')
 
-	
+
 	
 	# for url in questionUrls:
-	# 	scrapper.url_to_soup(url, meta)
+	# 	scraper.url_to_soup(url, meta)
 
 get_all_featured_questions(meta)
